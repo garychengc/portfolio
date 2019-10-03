7d44fc772932b0607fd3544cbdc71c4bf3b95df8
GC <gc@GCs-MacBook-Pro.local>
2019-10-02 21:11:22 -0400

    added a tag for nav bar

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,11 +8,11 @@ class Header extends React.Component {
         return (
             <header id="header">
                 <div className="inner">
-                    <a href="#" className="image avatar"><img src={avatar} alt="" /></a>
+                    <a href="#one" className="image avatar"><img src={avatar} alt="" /></a>
                     <div id="side-nav">
-                        <div id="about-me">About Me</div>
-                        <div id="projects">Projects</div>
-                        <div id="contact-me">Contact Me</div>
+                        <a href="#one"><div id="about-me">About Me</div> </a>
+                        <a href="#two"><div id="projects">My Projects</div></a>
+                        <a href="#three"><div id="contact-me">Contact Me</div></a>
 
                     </div>
                 </div>
